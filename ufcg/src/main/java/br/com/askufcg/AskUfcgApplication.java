cdca9bae875dce88c0a46f662bcb8c71be4ad986
Lucian <lucian.costa.ext@synchro.com.br>
2021-07-17 17:08:29 -0300

    criando a base do backend

---
+++ ufcg/src/main/java/br/com/askufcg/AskUfcgApplication.java
@@ -0,0 +1,13 @@
+package br.com.askufcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskUfcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskUfcgApplication.class, args);
+	}
+
+}
---
+++ ufcg/src/test/java/br/com/askufcg/AskUfcgApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.askufcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskUfcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a96d4d552750d2bbd457fd88dff16fbe990cd493
Wellisson Gomes <wellisson.cacho@signove.com>
2021-08-03 21:56:48 -0300

    fix questions order by default

--- ufcg/src/main/java/br/com/askufcg/services/search/QuestionSearchServiceImpl.java
+++ ufcg/src/main/java/br/com/askufcg/services/search/QuestionSearchServiceImpl.java
@@ -6,10 +6,7 @@ import br.com.askufcg.repositories.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -31,6 +28,7 @@ public class QuestionSearchServiceImpl implements QuestionSearchService {
                                 .collect(Collectors.toList());
         }
 
+        Collections.reverse(questions);
         if(!NEW.equals(filter)) {
             questions = updateQuestionsByFilter(questions, filter);
         }

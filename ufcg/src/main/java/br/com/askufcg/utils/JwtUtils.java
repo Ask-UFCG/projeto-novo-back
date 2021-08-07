7b334904d4a7c97a54f688cf074414b11804242f
Wellisson Gomes <wellisson.cacho@signove.com>
2021-08-07 17:18:03 -0300

    create constant in JwtUtils

--- ufcg/src/main/java/br/com/askufcg/utils/JwtUtils.java
+++ ufcg/src/main/java/br/com/askufcg/utils/JwtUtils.java
@@ -3,6 +3,7 @@ package br.com.askufcg.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +14,10 @@ import java.util.function.Function;
 
 @Component
 public class JwtUtils {
-    private String secret = "learnitcodeit";
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private static final Date TWO_HOURS = new Date(System.currentTimeMillis() + 60 * 60 * 1000 * 2);
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -44,7 +48,7 @@ public class JwtUtils {
     private String createToken(Map<String, Object> claims, String subject) {
 
         return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .setExpiration(TWO_HOURS)
                 .signWith(SignatureAlgorithm.HS256, secret).compact();
     }
 
